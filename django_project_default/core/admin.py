b5d509016f81348239aad2b6f6d6bd3c7b2de9bc
yasunt <kistat.info@gmail.com>
2017-05-28 14:23:46 +0900

    [add] Add model admin

--- django_project_default/core/admin.py
+++ django_project_default/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from something.models import Something, Tag
+
+
+class SomethingAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'user', 'created', 'updated')
+    readonly_fields = ('user', 'created', 'updated')
+    date_hierarchy = 'created'
+
+
+class TagAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('created', 'updated')
+    date_hierarchy = 'created'
+
+
+admin.site.register(Something, SomethingAdmin)
+admin.site.register(Tag, TagAdmin)
